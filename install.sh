bb1a486681e71a6f050edfdc25d15ad50f3e4e7f
Niko Steinhoff <niko.steinhoff@gmail.com>
2019-04-24 10:20:23 +0200

    Backup old files

--- install.sh
+++ install.sh
@@ -12,5 +12,15 @@ fi
 
 files=(init/init.txt init/d_init.txt init/interface.txt art/taffer_20x20.png)
 for file in ${files[*]}; do
+    src="$(pwd)/$file"
+    target="$dir/$file"
+    if [ -L "$target" ]; then
+        echo "Removing old link '$target'"
+        rm "$target"
+    fi
+    if [ -f "$target" ]; then
+        echo "Backing up old file '$target'"
+        mv "$target" "$target"_"$(date -Ins)"
+    fi
     ln -v -s -f "$(pwd)/$file" "$dir/$file"
 done
